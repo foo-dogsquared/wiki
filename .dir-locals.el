7caf465900b80f72e330a44b4696755b00960f3f
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-04-19 22:50:59 +0800

    Add project-specific settings

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((org-mode .
+        ((org-babel-default-header-args . ((:session . "none")
+                                           (:results . "output")
+                                           (:exports . "both")
+                                           (:cache . "yes")
+                                           (:noweb . "no")
+                                           (:hlines . "no")
+                                           (:tangle . "no")))
+         (org-babel-results-keyword . "results"))))
