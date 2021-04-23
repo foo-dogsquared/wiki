11391485f68c3bbbb076ee12e3fc49ba410ecaca
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-04-24 01:08:53 +0800

    Update project-specific config

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,17 @@
 ((org-mode .
-        ((org-babel-default-header-args . ((:session . "none")
+         ((org-babel-default-header-args . ((:session . "none")
                                            (:results . "replace output")
                                            (:exports . "both")
                                            (:cache . "no")
                                            (:noweb . "yes")
+                                           (:mkdirp . "yes")
                                            (:tangle . "no")))
-         (org-babel-results-keyword . "results")
-         (org-export-with-toc . t))))
+          (org-babel-default-header-args:org . ((:results . "silent")
+                                                (:exports . "code")))
+          (eval . (setq org-babel-default-header-args
+                        (cons `(:dir . ,(concat
+                                         (file-name-directory (buffer-file-name))
+                                         "assets/"
+                                         (file-name-base (buffer-file-name))))
+                              (assq-delete-all :dir org-babel-default-header-args))))
+          (org-babel-results-keyword . "results"))))
