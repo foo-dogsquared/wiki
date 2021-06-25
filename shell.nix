e11eee8613dce11824c7af2dfefcf1b17c287faa
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-06-25 13:32:38 +0800

    Update docs, local env, references, and my index
    In that order...

--- shell.nix
+++ shell.nix
@@ -1,13 +1,20 @@
-{ pkgs ? import (fetchTarball "http://nixos.org/channels/nixos-20.09/nixexprs.tar.xz") {} }:
+{ pkgs ? import (fetchTarball "http://nixos.org/channels/nixos-21.05/nixexprs.tar.xz") {} }:
 
+let
+  python3WithPackages = pkgs.python3.withPackages(p:
+    with pkgs.python3Packages; [
+      pynvim
+    ]);
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gnumake
     gnuplot
     graphviz
     lilypond
+    nodejs-14_x
     octaveFull
-    python3
+    python3WithPackages
     racket
     R
     recoll
