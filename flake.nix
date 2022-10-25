c6d901c5b2808fb6fc52978f5bff94bdfeaec23c
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-10-25 16:50:51 +0800

    Update devshell to follow nixpkgs unstable branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,25 @@
 {
-  description = "The flake for this project.";
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    devShell.x86_64-linux = import ./shell.nix { pkgs = import nixpkgs { system = "x86_64-linux"; }; };
+  description = "The flake for this digital notebook.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    foo-dogsquared-nixos-config.url = "github:foo-dogsquared/nixos-config";
+    foo-dogsquared-nixos-config.inputs.nixpkgs.follows = "nixpkgs";
+    foo-dogsquared-nixos-config.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: let
+    overlays = [
+      inputs.foo-dogsquared-nixos-config.overlays.default
+    ];
+    defaultSystem = inputs.flake-utils.lib.system.x86_64-linux;
+    systems = with inputs.flake-utils.lib.system; [ defaultSystem ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs { inherit system overlays; };
+    in
+      { default = import ./shell.nix { inherit pkgs; }; });
   };
 }
